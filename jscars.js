6e6c910629c7f658aa0ac28cce03b5277b2077b2
Abdulbasid8900 <97109152@st.deltion.nl>
2024-06-17 13:04:51 +0200

    first commit

---
+++ jscars.js
@@ -0,0 +1,278 @@
+
+
+const aCars = [
+    {
+        "cartype": "SUV",
+        "license_plate": "ABC123",
+        "year": 2015,
+        "manufacturer": "Toyota",
+        "km": 75000
+    },
+    {
+        "cartype": "Sedan",
+        "license_plate": "XYZ456",
+        "year": 2012,
+        "manufacturer": "Honda",
+        "km": 40000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "DEF789",
+        "year": 2018,
+        "manufacturer": "Ford",
+        "km": 110000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "GHI123",
+        "year": 2016,
+        "manufacturer": "Chevrolet",
+        "km": 30000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "JKL456",
+        "year": 2019,
+        "manufacturer": "Volkswagen",
+        "km": 2000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "MNO789",
+        "year": 2014,
+        "manufacturer": "BMW",
+        "km": 80000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "PQR123",
+        "year": 2017,
+        "manufacturer": "Mercedes-Benz",
+        "km": 95000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "STU456",
+        "year": 2013,
+        "manufacturer": "Audi",
+        "km": 70000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "VWX789",
+        "year": 2010,
+        "manufacturer": "Lexus",
+        "km": 100000
+    },
+    {
+        "cartype": "Sedan",
+        "license_plate": "YZA123",
+        "year": 2011,
+        "manufacturer": "Nissan",
+        "km": 60000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "BCD789",
+        "year": 2013,
+        "manufacturer": "Toyota",
+        "km": 58000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "EFG123",
+        "year": 2016,
+        "manufacturer": "BMW",
+        "km": 72000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "HIJ789",
+        "year": 2019,
+        "manufacturer": "Ford",
+        "km": 25000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "KLM123",
+        "year": 2015,
+        "manufacturer": "Honda",
+        "km": 87000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "NOP789",
+        "year": 2018,
+        "manufacturer": "Chevrolet",
+        "km": 32000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "QRS123",
+        "year": 2014,
+        "manufacturer": "Volkswagen",
+        "km": 41000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "TUV789",
+        "year": 2017,
+        "manufacturer": "BMW",
+        "km": 67000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "VWX123",
+        "year": 2018,
+        "manufacturer": "Mercedes-Benz",
+        "km": 93000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "YZA789",
+        "year": 2016,
+        "manufacturer": "Audi",
+        "km": 78000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "BCD123",
+        "year": 2012,
+        "manufacturer": "Lexus",
+        "km": 105000
+    },
+    {
+        "cartype": "Sedan",
+        "license_plate": "EFG789",
+        "year": 2014,
+        "manufacturer": "Nissan",
+        "km": 69000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "FGH123",
+        "year": 2019,
+        "manufacturer": "Toyota",
+        "km": 54000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "IJK789",
+        "year": 2016,
+        "manufacturer": "Volkswagen",
+        "km": 62000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "LMN123",
+        "year": 2011,
+        "manufacturer": "BMW",
+        "km": 88000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "OPQ789",
+        "year": 2017,
+        "manufacturer": "Ford",
+        "km": 47000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "RST123",
+        "year": 2013,
+        "manufacturer": "Honda",
+        "km": 72000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "UVW789",
+        "year": 2018,
+        "manufacturer": "Chevrolet",
+        "km": 38000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "XYZ123",
+        "year": 2015,
+        "manufacturer": "Volkswagen",
+        "km": 68000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "456789",
+        "year": 2012,
+        "manufacturer": "BMW",
+        "km": 110000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "987654",
+        "year": 2019,
+        "manufacturer": "Ford",
+        "km": 29000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "321098",
+        "year": 2014,
+        "manufacturer": "Toyota",
+        "km": 63000
+    },
+    {
+        "cartype": "Sedan",
+        "license_plate": "13579",
+        "year": 2016,
+        "manufacturer": "Nissan",
+        "km": 54000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "753951",
+        "year": 2011,
+        "manufacturer": "Volkswagen",
+        "km": 89000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "246810",
+        "year": 2018,
+        "manufacturer": "BMW",
+        "km": 45000
+    },
+    {
+        "cartype": "Truck",
+        "license_plate": "579135",
+        "year": 2015,
+        "manufacturer": "Ford",
+        "km": 76000
+    },
+    {
+        "cartype": "SUV",
+        "license_plate": "480392",
+        "year": 2013,
+        "manufacturer": "Toyota",
+        "km": 84000
+    },
+    {
+        "cartype": "Hatchback",
+        "license_plate": "135792",
+        "year": 2017,
+        "manufacturer": "Chevrolet",
+        "km": 50000
+    },
+    {
+        "cartype": "Convertible",
+        "license_plate": "579321",
+        "year": 2019,
+        "manufacturer": "Volkswagen",
+        "km": 36000
+    },
+    {
+        "cartype": "Coupe",
+        "license_plate": "802467",
+        "year": 2016,
+        "manufacturer": "BMW",
+        "km": 69000
+    }
+]
+
---
+++ names.js
@@ -0,0 +1,23 @@
+
+    const names = [
+    { "firstName": "David", "infix": "von", "prefix": "Mrs.", "lastName": "Moore", "date_of_birth": "1987-03-15" },
+    { "firstName": "Ivy", "infix": "de la", "prefix": "Miss", "lastName": "Jones", "date_of_birth": "1992-08-25" },
+    { "firstName": "Bob", "infix": "van", "prefix": "Ms.", "lastName": "Williams", "date_of_birth": "1978-11-12" },
+    { "firstName": "Jack", "infix": "", "prefix": "Prof.", "lastName": "Miller", "date_of_birth": "1985-06-30" },
+    { "firstName": "Emma", "infix": "", "prefix": "Mr.", "lastName": "Smith", "date_of_birth": "1990-02-05" },
+    { "firstName": "Charlie", "infix": "der", "prefix": "Dr.", "lastName": "Taylor", "date_of_birth": "1983-09-20" },
+    { "firstName": "Alice", "infix": "del", "prefix": "Miss", "lastName": "Brown", "date_of_birth": "1976-04-18" },
+    { "firstName": "Grace", "infix": "van der", "prefix": "Mrs.", "lastName": "Wilson", "date_of_birth": "1989-12-10" },
+    { "firstName": "Frank", "infix": "de", "prefix": "Ms.", "lastName": "Johnson", "date_of_birth": "1981-07-03" },
+    { "firstName": "Henry", "infix": "", "prefix": "Mr.", "lastName": "Davis", "date_of_birth": "1995-10-28" },
+    { "firstName": "David", "infix": "van den", "prefix": "Dr.", "lastName": "Taylor", "date_of_birth": "1988-11-02" },
+    { "firstName": "Emma", "infix": "", "prefix": "Miss", "lastName": "Williams", "date_of_birth": "1993-06-15" },
+    { "firstName": "Ivy", "infix": "de", "prefix": "Mrs.", "lastName": "Moore", "date_of_birth": "1980-09-23" },
+    { "firstName": "Alice", "infix": "", "prefix": "Dr.", "lastName": "Jones", "date_of_birth": "1997-05-07" },
+    { "firstName": "Jack", "infix": "de la", "prefix": "Prof.", "lastName": "Brown", "date_of_birth": "1984-08-12" },
+    { "firstName": "Charlie", "infix": "van der", "prefix": "Mr.", "lastName": "Johnson", "date_of_birth": "1979-02-28" },
+    { "firstName": "Bob", "infix": "", "prefix": "Ms.", "lastName": "Smith", "date_of_birth": "1986-01-14" },
+    { "firstName": "Grace", "infix": "", "prefix": "Miss", "lastName": "Wilson", "date_of_birth": "1994-07-20" },
+    { "firstName": "Frank", "infix": "de", "prefix": "Mrs.", "lastName": "Miller", "date_of_birth": "1982-04-05" },
+    { "firstName": "Henry", "infix": "van den", "prefix": "Dr.", "lastName": "Davis", "date_of_birth": "1998-03-18" }
+]
